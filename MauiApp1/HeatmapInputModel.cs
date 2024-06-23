443f0de891894f26a266e6bb906ba621f2ffbd00
KaziMithfa <kazimithfa99@gmail.com>
2024-06-23 14:41:02 +0200

    Uploading the software Engineering Project all codes and documents

---
+++ MauiApp1/HeatmapInputModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components.Forms;
+
+public class HeatmapInputModel
+{
+    public IBrowserFile CsvFile { get; set; }
+    public string CsvContent { get; set; }
+    public bool UseFileInput { get; set; } = true;
+
+    [Required(ErrorMessage = "Highlight touch is required.")]
+    public int HighlightTouch { get; set; }
+
+    [Required(ErrorMessage = "Figure name is required.")]
+    public string FigureName { get; set; }
+
+    [Required(ErrorMessage = "X Axis label is required.")]
+    public string XAxisTitle { get; set; }
+
+    [Required(ErrorMessage = "Y Axis label is required.")]
+    public string YAxisTitle { get; set; }
+
+    public int? MaxCycles { get; set; }
+    public int? MinCell { get; set; }
+    public int? MaxCell { get; set; }
+    public int? MinTouch { get; set; }
+    public int? MaxTouch { get; set; }
+    public bool IsHorizontal { get; set; }
+}
