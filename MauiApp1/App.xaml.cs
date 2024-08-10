6177271f28385ddc76ffd1cf632981aa6f2898c8
saleque143 <saleque.ahmed@stud.fra-uas.de>
2024-08-10 16:00:00 +0200

    Code clean up for adding new features

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,22 +1,11 @@
-﻿using Microsoft.Maui.Controls;
-using System.Threading.Tasks;
-namespace MauiApp1
+﻿namespace MauiApp1
 {
 	public partial class App : Application
 	{
 		public App()
 		{
 			InitializeComponent();
-
 			MainPage = new MainPage();
-			Task.Run(async () => await MauiProgram.UploadHelloWorldToAzure());
-		}
-		protected override void OnStart()
-		{
-			base.OnStart();
-
-			// You could also call it here if you prefer
-			Task.Run(async () => await MauiProgram.UploadHelloWorldToAzure());
 		}
 	}
 }
